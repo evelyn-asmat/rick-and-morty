94f030f326d2ace1e6ed4986bf0974617061ce97
Evelyn Asmat <dgiesp112@minsa.gob.pe>
2023-11-18 04:03:55 -0500

    Estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,14 @@ export default function Card(props) {
    return (
       <div className="card">
          <button className="close-btn" onClick={() => props.onClose(props.id)}>X</button>
-         <h2>{props.status}</h2>
-         <Link to={`/detail/${props.id}`}>
-            <h2 className="card-name">{props.name}</h2>
-         </Link>
-         <h2>{props.gender}</h2>
-         <h2>{props.origin}</h2>
          <div className='wrap-img'>
             <img src={props.image} alt='' className="character-img"/>
          </div>
+         <Link to={`/detail/${props.id}`} className="character-name">
+            <h2>{props.name}</h2>
+         </Link>
+         <p>{props.status} | {props.gender}</p>
+         <p><b>{props.origin}</b></p>
       </div>
    );
 }
