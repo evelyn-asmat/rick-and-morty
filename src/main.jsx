5d8ec5df3a6c46269bda83bfbf0331853e51c2e1
Evelyn Asmat <dgiesp112@minsa.gob.pe>
2023-11-25 02:11:46 -0500

    Estilos Sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './styles/styles.scss';
+
+import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
-import store from './redux/store';
 import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
