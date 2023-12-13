bd9ff1a4ee697cb4d24f732b4cdf9b2f84c92f62
Evelyn Asmat <dgiesp112@minsa.gob.pe>
2023-12-13 01:06:22 -0500

    DB Connection

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,12 @@
+const { conn } = require('./DB_connection');
 const server = require("./app");
 const PORT = 3001;
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+conn
+   .sync({force: true})
+   .then(()=>{
+      server.listen(PORT, () => {
+         console.log('Server raised in port: ' + PORT);
+      });
+   })
+   .catch((err) => console.log(err));
